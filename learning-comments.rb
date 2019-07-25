c249986c3f54b2ac5435885b1c271f67577b51c6
Ian Harold <ianhunterharold@gmail.com>
2019-07-25 22:46:55 +0000

    Done.

--- learning-comments.rb
+++ learning-comments.rb
@@ -1,7 +1,9 @@
 #coments in rb
-run_code_inside = true
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
+
+
+#run_code_inside = false;
+#puts "Code before if...end"
+#if run_code_inside
+#puts "code inside"
+#end 
+#puts "Code after if...end"
